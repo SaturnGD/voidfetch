6a80718fbd03950b7de24f4f7d024f6d9b1973f5
null <saturngdformerlysye@gmail.com>
2024-10-14 13:10:24 -0400

    why wont it let me get name of cpu :(

--- source/Main.py
+++ source/Main.py
@@ -1,6 +1,12 @@
 import os
+import platform
+from cpuinfo import get_cpu_info
+
+cpu = get_cpu_info()
 
 # this shi already looks like yandere dev's code LMAOOO
 print("Operating System: " + os.name)
+print("System: " + platform.system())
 print("User: " + os.getlogin())
 print("DE: " + os.environ.get('DESKTOP_SESSION'))
+print("CPU: " + cpu)
