8c7355bfb26536889268f6ec054f49b5667062a4
null <saturngdformerlysye@gmail.com>
2024-10-14 12:59:06 -0400

    uploaded the main file

---
+++ source/Main.py
@@ -0,0 +1,6 @@
+import os
+
+# this shi already looks like yandere dev's code LMAOOO
+print("Operating System: " + os.name)
+print("User: " + os.getlogin())
+print("DE: " + os.environ.get('DESKTOP_SESSION'))
